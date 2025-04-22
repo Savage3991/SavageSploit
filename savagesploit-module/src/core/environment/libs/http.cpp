0c3011eb5a6ca53848c0048a791cb844066f2c19
savage <122698051+Savage3991@users.noreply.github.com>
2025-04-22 07:54:34 +0200

    Improve request

--- savagesploit-module/src/core/environment/libs/http.cpp
+++ savagesploit-module/src/core/environment/libs/http.cpp
@@ -2,13 +2,14 @@
 // Created by savage on 18.04.2025.
 //
 
+#include "lgc.h"
 #include "../environment.h"
 #include "cpr/api.h"
 #include "cpr/response.h"
 #include "httpstatus/httpstatus.h"
 #include "nlohmann/json.h"
 
-enum RequestMethods {
+enum request_methods {
 	H_GET,
 	H_HEAD,
 	H_POST,
@@ -17,7 +18,7 @@ enum RequestMethods {
 	H_OPTIONS
 };
 
-std::map<std::string, RequestMethods> Methods = {
+std::map<std::string, request_methods> methods = {
 	{ "get", H_GET },
 	{ "head", H_HEAD },
 	{ "post", H_POST },
@@ -32,40 +33,34 @@ int request(lua_State* L) {
 
 	lua_getfield(L, 1, "Url");
 	if (lua_type(L, -1) != LUA_TSTRING)
-		luaL_error(L, "The 'Url' field in the request table is either invalid or missing.");
+		luaL_error(L, "the url field is either missing or invalid");
 
-	std::string Url = lua_tostring(L, -1);
-
-	if (Url.empty())
-		luaL_error(L, "no url provided to request");
-
-	if (Url.find("http://") != 0 && Url.find("https://") != 0)
-		luaL_error(L, "The specified protocol is invalid (expected 'http://' or 'https://')");
+	std::string url = lua_tostring(L, -1);
+	if (url.empty())
+		luaL_error(L, "no url provided");
 
 	lua_pop(L, 1);
 
-	auto Method = H_GET;
+	auto method = H_GET;
 
 	lua_getfield(L, 1, "Method");
 	if (lua_type(L, -1) == LUA_TSTRING) {
-		std::string MethodString = luaL_checkstring(L, -1);
-		std::transform(MethodString.begin(), MethodString.end(), MethodString.begin(), tolower);
+		std::string method_string = luaL_checkstring(L, -1);
+		std::transform(method_string.begin(), method_string.end(), method_string.begin(), tolower);
 
-		if (!Methods.count(MethodString))
-			luaL_error(L, "The request type '%s' is not a valid HTTP request method.", MethodString.c_str());
+		if (!methods.count(method_string))
+			luaL_error(L, "the request type '%s' is not valid", method_string.c_str());
 
-		Method = Methods[MethodString];
+		method = methods[method_string];
 	}
 
 	lua_pop(L, 1);
 
-    auto Headers = std::map<std::string, std::string, cpr::CaseInsensitiveCompare>();
-	//Headers("Exploit-Guid", HardwareIdentifier);
-	//Headers("Nova-Fingerprint", HardwareIdentifier);
-    //Headers["Exploit-Guid"] = HardwareIdentifier;
-    //Headers["Nova-Fingerprint"] = HardwareIdentifier;
+    auto headers = std::map<std::string, std::string, cpr::CaseInsensitiveCompare>();
+    //Headers["Exploit-Guid"] = gw;
+    //Headers["Nova-Fingerprint"] = gw;
 
-	bool HasUserAgent = false;
+	bool user_agent = false;
 
 	lua_getfield(L, 1, "Headers");
 
@@ -73,14 +68,13 @@ int request(lua_State* L) {
 		lua_pushnil(L);
 		while (lua_next(L, -2) != 0) {
 			if (lua_type(L, -2) == LUA_TSTRING && lua_type(L, -1) == LUA_TSTRING) {
-				std::string Key = lua_tostring(L, -2);
-				std::string Value = lua_tostring(L, -1);
+				std::string key = lua_tostring(L, -2);
+				std::string value = lua_tostring(L, -1);
 
-				if (Key == "User-Agent")
-					HasUserAgent = true;
+				if (key == "User-Agent")
+					user_agent = true;
 
-                Headers[Key] = Value;
-				//Headers.Insert(Key, Value);
+                user_agent[key] = value;
 			}
 			lua_pop(L, 1);
 		}
@@ -88,103 +82,106 @@ int request(lua_State* L) {
 
 	lua_pop(L, 1);
 
-	if (!HasUserAgent)
-        Headers["User-Agent"] = "Nova/1.0";
+	if (!user_agent)
+        headers["User-Agent"] = "Nova/1.0";
 
 	lua_getglobal(L, "game");
 	lua_getfield(L, -1, "JobId");
-	std::string jobId = lua_tostring(L, -1);
+	std::string job_id = lua_tostring(L, -1);
 	lua_pop(L, 2);
 
 	using Json = nlohmann::json;
 	Json SessionId;
-	SessionId["GameId"] = jobId.c_str();
-	SessionId["PlaceId"] = jobId.c_str();
+	SessionId["GameId"] = job_id.c_str();
+	SessionId["PlaceId"] = job_id.c_str();
 
-    Headers["Roblox-Session-Id"] = SessionId.dump();
-    Headers["Roblox-Place-Id"] = jobId;
-    Headers["Roblox-Game-Id"] = jobId;
-    Headers["ExploitIdentifier"] = "Nova";
+    headers["Roblox-Session-Id"] = SessionId.dump();
+    headers["Roblox-Place-Id"] = job_id;
+    headers["Roblox-Game-Id"] = job_id;
+    headers["ExploitIdentifier"] = "Nova";
 
-    cpr::Cookies Cookies;
+    cpr::Cookies cookies;
 	lua_getfield(L, 1, "Cookies");
 	if (lua_type(L, -1) == LUA_TTABLE) {
 		lua_pushnil(L);
 		while (lua_next(L, -2) != 0) {
             if (!lua_isstring(L, -2) || !lua_isstring(L, -1)) {
-                luaL_argerrorL(L, 1, "Expected Valid Cookie Key and Value");
+                luaL_argerrorL(L, 1, "expected valid cookie key and value");
             }
 
-            Cookies.emplace_back(cpr::Cookie{_strdup(lua_tostring(L, -2)), _strdup(lua_tostring(L, -1))});
+            cookies.emplace_back(cpr::Cookie{_strdup(lua_tostring(L, -2)), _strdup(lua_tostring(L, -1))});
 
             lua_pop(L, 1);
 		}
 	}
 	lua_pop(L, 1);
 
-	std::string Body = "";
+	std::string body = "";
 	lua_getfield(L, 1, "Body");
 	if (lua_type(L, -1) == LUA_TSTRING)
-		Body = lua_tostring(L, -1);
+		body = lua_tostring(L, -1);
 
 	lua_pop(L, 1);
 
-    cpr::Session RequestSession;
-    RequestSession.SetUrl(Url);
-    RequestSession.SetHeader(cpr::Header{Headers});
-    RequestSession.SetCookies(Cookies);
-    RequestSession.SetBody(Body);
+    cpr::Session request_session;
+    request_session.SetUrl(url);
+    request_session.SetHeader(cpr::Header{headers});
+    request_session.SetCookies(cookies);
+    request_session.SetBody(body);
 
-    cpr::Response Response;
+    cpr::Response response;
 
-    switch (Method) {
+    switch (method) {
         case H_GET:
-            Response = RequestSession.Get();
+            response = request_session.Get();
             break;
-        case H_POST:
-            Response = RequestSession.Post();
+        case response:
+            response = request_session.Post();
             break;
         case H_PUT:
-            Response = RequestSession.Put();
+            response = request_session.Put();
             break;
         case H_DELETE:
-            Response = RequestSession.Delete();
+            response = request_session.Delete();
             break;
         default:
-            Response.status_code = -999;
+            response.status_code = -999;
             break;
     }
 
-    if (Response.status_code == -999) {
-        luaL_error(L, "The specified request method is invalid.");
+    if (response.status_code == -999) {
+        luaL_error(L, "the request method is invalid");
     }
+
+	lua_rawcheckstack(L, 1);
+	luaC_threadbarrier(L);
 
     lua_newtable(L);
 
-    lua_pushboolean(L, !HttpStatus::IsError(Response.status_code) &&
-                       Response.status_code != 0);
+    lua_pushboolean(L, !HttpStatus::IsError(response.status_code) &&
+                       response.status_code != 0);
     lua_setfield(L, -2, "Success");
 
-    lua_pushinteger(L, Response.status_code);
+    lua_pushinteger(L, response.status_code);
     lua_setfield(L, -2, "StatusCode");
 
-    lua_pushstring(L, Response.text.c_str());
+    lua_pushstring(L, response.text.c_str());
     lua_setfield(L, -2, "Body");
 
-    lua_pushstring(L, Response.status_line.c_str());
+    lua_pushstring(L, response.status_line.c_str());
     lua_setfield(L, -2, "StatusMessage");
 
     lua_newtable(L);
-    for (const auto &Header: Response.header) {
-        lua_pushstring(L, Header.second.c_str());
-        lua_setfield(L, -2, Header.first.c_str());
+    for (const auto &header: response.header) {
+        lua_pushstring(L, header.second.c_str());
+        lua_setfield(L, -2, header.first.c_str());
     }
     lua_setfield(L, -2, "Headers");
 
     lua_newtable(L);
-    for (const auto &Cookie: Response.cookies) {
-        lua_pushstring(L, Cookie.GetValue().c_str());
-        lua_setfield(L, -2, Cookie.GetName().c_str());
+    for (const auto &cookie: response.cookies) {
+        lua_pushstring(L, cookie.GetValue().c_str());
+        lua_setfield(L, -2, cookie.GetName().c_str());
     }
     lua_setfield(L, -2, "Cookies");
 
